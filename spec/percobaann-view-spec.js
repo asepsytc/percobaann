63589262d3fa3aa6c5902b0ec1f73635aa4a0b7c
asepsytc <asepsytc@gmail.com>
2022-07-19 18:55:38 +0700

    update

---
+++ spec/percobaann-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PercobaannView from '../lib/percobaann-view';
+
+describe('PercobaannView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
